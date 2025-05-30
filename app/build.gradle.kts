e503c1d2ad6e3716872a73e2fd968d1953b58b08
Rafael Marcos <rafix995@gmail.com>
2025-05-30 19:46:24 +0100

    Modified API level to 26 to support java.time.LocalDate

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "dev.rmarcos.performancereviewapp"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
