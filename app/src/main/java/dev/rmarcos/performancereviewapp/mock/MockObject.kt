1fde85754c3d87ed4e2bfc2392eca93f7dffe695
Rafael Marcos <rafix995@gmail.com>
2025-05-30 19:47:48 +0100

    Created Mock Object to mock data into the app

---
+++ app/src/main/java/dev/rmarcos/performancereviewapp/mock/MockObject.kt
@@ -0,0 +1,107 @@
+package dev.rmarcos.performancereviewapp.mock
+
+import dev.rmarcos.performancereviewapp.model.*
+import java.time.LocalDate
+import java.util.UUID
+
+val mockObject = object {
+    private val department1 = Department (
+        id = 1,
+        name = "IT",
+        description = "Responsible to maintain the company-s IT infrastructure"
+    )
+    private val department2 = Department (
+        id = 2,
+        name = "HR",
+        description = "Human Resources Department"
+    )
+    private val developmentPlan1 = DevelopmentPlan (
+        id = 1,
+        startDate = LocalDate.now(),
+        endDate = LocalDate.now().plusMonths(6),
+        department = department1
+    )
+    private val developmentPlan2 = DevelopmentPlan (
+        id = 2,
+        startDate = LocalDate.now().minusMonths(4),
+        endDate = LocalDate.now().plusMonths(2),
+        department = department2
+    )
+    private val goal1 = Goal (
+        id = 1,
+        title = "New Infrastructure",
+        description = "Build a new infrastructure for the new installations",
+        weight = 2,
+        points = 80,
+        state = GoalState.DONE,
+        developmentPlan = developmentPlan1
+    )
+    private val goal2 = Goal (
+        id = 2,
+        title = "Software Upgrade",
+        description = "Upgrade used softwares",
+        weight = 1,
+        points = 20,
+        state = GoalState.DONE,
+        developmentPlan = developmentPlan1
+    )
+    private val goal3 = Goal (
+        id = 3,
+        title = "Increase IT cew",
+        description = "Due to the new installations, we'll need more IT experts",
+        weight = 3,
+        points = 75,
+        state = GoalState.DONE,
+        developmentPlan = developmentPlan2
+    )
+    private val assessment1 = Assessment (
+        id = 1,
+        developmentPlan = developmentPlan1,
+        score = (goal1.weight * goal1.points) + (goal2.weight * goal2.points),
+        date = LocalDate.now(),
+        minimalSuccessScore = 150
+    )
+    private val assessment2 = Assessment (
+        id = 2,
+        developmentPlan = developmentPlan2,
+        score = (goal2.weight * goal2.points),
+        date = LocalDate.now(),
+        minimalSuccessScore = 200
+    )
+    private val profile1 = Profile (
+        id = 1,
+        name = "Leopoldo Mandume",
+        phone = "+244 9XX XXX XXX",
+        altPhone = "+244 9XX XXX XXX",
+        role = "Gestor de IT",
+        department = department1
+    )
+    private val profile2 = Profile (
+        id = 1,
+        name = "Ngola Samuel",
+        phone = "+244 9XX XXX XXX",
+        altPhone = "+244 9XX XXX XXX",
+        role = "Gestor de RH",
+        department = department2
+    )
+    private val user1 = User (
+        uuid = UUID.randomUUID().toString(),
+        username = "lmandume",
+        email = "user@domain.com",
+        password = "12345",
+        permission = Permission.MANAGER,
+        profile = profile1
+    )
+    private val user2 = User (
+        uuid = UUID.randomUUID().toString(),
+        username = "nsamuel",
+        email = "user@domain.com",
+        password = "12345",
+        permission = Permission.MANAGER,
+        profile = profile2
+    )
+    fun users(): List<User> = listOf(user1, user2)
+    fun developmentPlans(): List<DevelopmentPlan> = listOf(developmentPlan1, developmentPlan2)
+    fun goals(): List<Goal> = listOf(goal1, goal2, goal3)
+    fun assessments(): List<Assessment> = listOf(assessment1, assessment2)
+}
